28e206adbd1b617c6c42fd6ad5ac19c99cb33734
Vladislav Tereshkin <firstaake@gmail.com>
2020-04-09 02:32:36 +0300

    implements observer panel component

---
+++ src/components/ObserverPanel.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Menu />
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import Menu from './components/Menu/Menu'
+
+export default {
+  name: 'ObserverPanel',
+  components: {
+    Menu
+  }
+}
+</script>
