7dfb15469cc056a5ac979b6f2bf36d83362efa92
Vladislav Tereshkin <firstaake@gmail.com>
2020-04-09 02:23:47 +0300

    implements not found component

---
+++ src/components/NotFound.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p>Такой страницы не существует!</p>
+    <router-link to="/">На главную</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFound',
+}
+</script>
+
+<style scoped>
+div {
+  min-height: 100vh;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-weight: 500;
+  font-size: 18px;
+}
+</style>
